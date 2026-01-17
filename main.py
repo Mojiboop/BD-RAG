e43ebe44762142eedcb24110b3171509b43b2459
Mojiboop <git@moji.rip>
2026-01-17 14:48:28 +0000

    Housekeeping. use main function in main.py, make use of if __name__ stuff

--- main.py
+++ main.py
@@ -4,37 +4,38 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise ValueError("Please input your api key into .env")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise ValueError("Please input your api key into .env")
 
-client = genai.Client(api_key=api_key)
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
-messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=messages,
-    )
-usage = response.usage_metadata
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=messages,
+        )
+    usage = response.usage_metadata
 
-if not usage:
-    raise RuntimeError("Gemini API response appears to be missing usage metadata")
+    if not usage:
+        raise RuntimeError("Gemini API response appears to be missing usage metadata")
 
-if args.verbose == True:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
-    print(f"Response:\n{response.text}")
-else:
-    print(f"Response:\n{response.text}")
-
-
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
+        print(f"Response:\n{response.text}")
+    else:
+        print(f"Response:\n{response.text}")
 
 
-#if __name__ == "__main__":
-#   main()
+
+
+if __name__ == "__main__":
+   main()
